2b5bf62fdf19504aace101bd9e52b7ccc2b1cb5e
FranzCSUF <fsalvador79@gmail.com>
2018-10-18 10:25:40 -0700

    Adding display slides function.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 /* eslint-disable no-unused-vars */
+
+let slideIndex = 0
+
+displaySlides(slideIndex)
+
+const $slides = document.getElementsByClassName('slide')
+
+function displaySlides() {
+  const $slides = document.getElementsByClassName('slide')
+  for (let i = 0; i < $slides.length; i++) {
+    $slides[i].style.display = 'none'
+  }
+  $slides[slideIndex].style.display = 'block'
+}

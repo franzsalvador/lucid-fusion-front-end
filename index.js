fb748ca26b9bf1cabc8964bf6930ec6413baad56
FranzCSUF <fsalvador79@gmail.com>
2018-10-18 10:27:07 -0700

    Adding prev and next slide functionality.

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ displaySlides(slideIndex)
 
 const $slides = document.getElementsByClassName('slide')
 
+const prevSlide = () => {
+  $slides[slideIndex].setAttribute('class', 'slide slideOutLeft')
+  setTimeout(() => {
+    slideIndex < 1
+      ? slideIndex = $slides.length - 1
+      : slideIndex = slideIndex -= 1
+    $slides[slideIndex].setAttribute('class', 'slide slideInLeft')
+    displaySlides()
+  }, 600)
+}
+
+const nextSlide = () => {
+  $slides[slideIndex].setAttribute('class', 'slide slideOutRight')
+  setTimeout(() => {
+    slideIndex >= $slides.length - 1
+      ? slideIndex = 0
+      : slideIndex = slideIndex += 1
+    $slides[slideIndex].setAttribute('class', 'slide slideInRight')
+    displaySlides()
+  }, 600)
+}
+
 function displaySlides() {
   const $slides = document.getElementsByClassName('slide')
   for (let i = 0; i < $slides.length; i++) {
